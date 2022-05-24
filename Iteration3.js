60fdb4d0399755ec4d2d6c25bcea1b20dd8cd1fc
victorleon0 <victor.leon.huerta@gmail.com>
2022-05-24 09:31:21 +0100

    Iteration 4

--- Iteration3.js
+++ Iteration3.js
@@ -1,7 +1,8 @@
 //3.1 Dado el siguiente array, crea una copia usando spread operators.
-const pointsList = [32, 54, 21, 64, 75, 43]
+const pointsList1 = [32, 54, 21, 64, 75, 43]
 
-const points = [...pointsList]
+const points = [...pointsList1]
+console.log(points)
 
 //3.2 Dado el siguiente objeto, crea una copia usando spread operators.
 const toy = {name: 'Bus laiyiar', date: '20-30-1995', color: 'multicolor'};
@@ -9,13 +10,13 @@ const toy = {name: 'Bus laiyiar', date: '20-30-1995', color: 'multicolor'};
 const juguete = {...name, ...date, ...color}
 
 //3.3 Dado los siguientes arrays, crea un nuevo array juntandolos usando spread operatos.
-const pointsList = [32, 54, 21, 64, 75, 43];
+const pointsList0 = [32, 54, 21, 64, 75, 43];
 const pointsLis2 = [54,87,99,65,32];
 
-const newarray = [...pointsList, ...pointsLis2]
+const newarray = [...pointsList0, ...pointsLis2]
 
 //3.4 Dado los siguientes objetos. Crea un nuevo objeto fusionando los dos  con spread operators.
-const toy = {name: 'Bus laiyiar', date: '20-30-1995', color: 'multicolor'};
+const toy2 = {name: 'Bus laiyiar', date: '20-30-1995', color: 'multicolor'};
 const toyUpdate = {lights: 'rgb', power: ['Volar like a dragon', 'MoonWalk']}
 
 const newarray2 = [...toy, ...toyUpdate]
